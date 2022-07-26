27c5674763aaa294b5b696edab69b11d90eafbf6
console.log(cat == cute) <qwreey75@gmail.com>
2022-07-27 02:10:41 +0900

    Update function.lua

--- libs/function.lua
+++ libs/function.lua
@@ -6,25 +6,25 @@ local format = string.format;
 
 local function normalFormatter(str,set)
 	if set == "-" then return format(" = function(%s)",str); end
-	return format("function(%s)",str);
+	return format(" function(%s)",str);
 end
 
 local function selfFormatter(str,set)
 	local comma = str:gsub("\n \t","") == "" and "" or ",";
 	if set == "=" then return format(" = function(self%s%s)",comma,str); end
-	return format("function(self%s%s)",comma,str);
+	return format(" function(self%s%s)",comma,str);
 end
 
 function module.arrow(str)
 	return gsub(gsub(gsub(gsub(gsub(gsub(gsub(gsub(str,
-		"%(([^%(%)%-%%%+=%?:'\\\"\n]-)%) ?%-(%-?)>",normalFormatter),
-		"([%w_]-) ?%-(%-?)>",normalFormatter),
-		"%(([^%(%)%-%%%+=%?:'\\\"\n]-)%) ?=(=?)>",selfFormatter),
-		"([%w_]-) ?=(=?)>",selfFormatter),
-		"%-%->"," = function()"),
-		"==>"," = function(self)"),
-		"%->"," function()"),
-		"=>"," function(self)"
+		" ?%(([^%(%)%-%%%+=%?:'\\\"\n]-)%) ?%-(%-?)>",normalFormatter),
+		" ?([%w_]-) ?%-(%-?)>",normalFormatter),
+		" ?%(([^%(%)%-%%%+=%?:'\\\"\n]-)%) ?=(=?)>",selfFormatter),
+		" ?([%w_]-) ?=(=?)>",selfFormatter),
+		" ?%-%->"," = function()"),
+		" ?==>"," = function(self)"),
+		" ?%->"," function()"),
+		" ?=>"," function(self)"
 	)
 end
 
