53b94423015180b7fd45b377c094c7fc343cbdb1
Qwreey <qwreey75@gmail.com>
2022-09-04 04:32:45 +0900

    self 에 대한 버그 픽스 (@ 만 입력해도 self. 되는 버그)

--- libs/self.lua
+++ libs/self.lua
@@ -2,18 +2,20 @@ local module = {};
 
 local gsub = string.gsub;
 
-local function selfFormatter(front,caller,all,st,mid,ed)
+local function selfFormatter(front,index,caller,all,st,mid,ed)
 	if (not front) or front == "" then return; end
-	if caller ~= "" and caller then
+	if index ~= "" and index then
+		return ("%sself.%s%s%s"):format(front,index,caller or "",all or "")
+	elseif caller ~= "" and caller then
 		return ("%sself%s%s"):format(front,caller,all or "")
 	elseif st == "[" then
 		return ("%sself%s"):format(front,all);
 	end
-	return ("%sself.%s"):format(front,all);
+	return ("%sself%s"):format(front,all);
 end
 
 function module.self(str)
-	return gsub(str,"([%)}%]; \n\t=%({%[]?)@([%.:]?)((%[?)([^ ]*)(%]?))",selfFormatter);
+	return gsub(str,"([%)}%]; \n\t=%({%[]?)@([%w_]*)([%.:]?)((%[?)([^ ]*)(%]?))",selfFormatter);
 end
 
 return module;
