0f6e9d3b201d9777ef6774ced29c762106a9aa5b
Qwreey <qwreey75@gmail.com>
2022-09-04 04:29:13 +0900

    self 에 : caller 추가

--- libs/self.lua
+++ libs/self.lua
@@ -2,16 +2,18 @@ local module = {};
 
 local gsub = string.gsub;
 
-local function selfFormatter(front,all,st,mid,ed)
+local function selfFormatter(front,caller,all,st,mid,ed)
 	if (not front) or front == "" then return; end
-	if st == "[" then
+	if caller ~= "" and caller then
+		return ("%sself%s%s"):format(front,caller,all or "")
+	elseif st == "[" then
 		return ("%sself%s"):format(front,all);
 	end
 	return ("%sself.%s"):format(front,all);
 end
 
 function module.self(str)
-	return gsub(str,"([%)}%]; \n\t=%({%[]?)@((%[?)([^ ]*)(%]?))",selfFormatter);
+	return gsub(str,"([%)}%]; \n\t=%({%[]?)@([%.:]?)((%[?)([^ ]*)(%]?))",selfFormatter);
 end
 
 return module;
